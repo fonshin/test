330e47d3b9cc94ee6b97146286532e20970ad4fe
fonshin <chengfeng.chu@qq.com>
2018-12-02 02:18:33 +0800

    build

---
+++ precache-manifest.d327ba250648e3e0cf3f1d6bf73327e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "63e4f481c6a4615ed6045ad94d529fae",
+    "url": "/static/media/a.63e4f481.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b88b7dbb7e3116d249f3",
+    "url": "/static/js/main.b88b7dbb.chunk.js"
+  },
+  {
+    "revision": "a9d6ba5203de63d53915",
+    "url": "/static/js/1.a9d6ba52.chunk.js"
+  },
+  {
+    "revision": "b88b7dbb7e3116d249f3",
+    "url": "/static/css/main.96e58e61.chunk.css"
+  },
+  {
+    "revision": "0636f7c2bdc3cefa818c567ced27e4b0",
+    "url": "/index.html"
+  }
+];
